72a9545ff6208bb0ef10f164f99678a3024acaa2
Yash Kumar <theyashkumar004@gmail.com>
2025-08-14 13:16:20 +0530

    deployment ready

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "https://chat-backend-yky8.onrender.com/api",
   withCredentials: true,
 });
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://chat-app-backend-mdog.onrender.com";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://chat-backend-yky8.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
